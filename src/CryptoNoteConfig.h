82dafe93fd671176ad4125ded94f28d82e83879f
k1456072jku <mark9090@gmx.at>
2017-05-11 05:47:08 +0200

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,7 +74,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "JKUTestCoin";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff000183b1de16029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121013262725c85916b61c99d6ce2ae6da263ce1f43741096678460a2dbf5159ae74a";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
